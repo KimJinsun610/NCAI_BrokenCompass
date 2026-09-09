84d3943a4e20e8ba5abbeb0597d4be2044c4f9d8
Jinsun Kim <realsun@tukorea.ac.kr>
2026-09-09 22:14:45 +0900

    Scene Change manager

--- horror house/Assets/3.2 Programmer_Kim/02 Scripts/SceneTransitionManager.cs
+++ horror house/Assets/3.2 Programmer_Kim/02 Scripts/SceneTransitionManager.cs
@@ -6,7 +6,19 @@ public class SceneTransitionManager : MonoBehaviour
 
     public void LoadScene(string sceneName)
     {
+        Debug.Log($"[SceneTransitionManager] 버튼 클릭됨! 이동할 씬: {sceneName}");
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("[SceneTransitionManager] 게임 종료");
+
+    #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit();
+    #endif
     }
 
 }
